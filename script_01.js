b511baf24b8d95146e94d6cfdd41e333527d95d6
Peter <schmidt.multimedia@gmail.com>
2023-04-17 12:13:34 +0200

    Objekte 1 Daten/Funktionen

--- script_01.js
+++ script_01.js
@@ -7,6 +7,7 @@
 // output(arr[0]);
 // output("------------");
 
+
 // // nested arrays - Prinzip: [[[]]]
 // // nested arrays  - hier: [[],[]]
 // arr =   [
@@ -31,3 +32,37 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+
+let person = {
+                firstName:"Evtim",
+                familyName:"Trenev",
+                salary: [120000,160000],
+                permission: true,       
+                sayHello:
+                        function(){
+                            return "Hallo " + this.firstName + "!";
+                        }
+            };
+
+
+output(person);
+output(person.firstName);
+output(person.familyName);
+output(person.salary[1]);
+output(person.permission);
+
+// const txt =     "Ich bin " + person.firstName + " " +
+//                  person.familyName +
+//                  " und verdiene " + 
+//                  person.salary[1] + " p.a";
+
+// output(txt);
+
+output(person.sayHello());
+
+
+
+/** Ausgabe */
+function output(outputData) {
+    console.log(outputData);
+}
